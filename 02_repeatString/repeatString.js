65187222fb530c380e82c63e0287d03fa2173a02
Incultavulpes <fernando.revenga@gmail.com>
2024-03-25 18:18:18 +0100

    Do exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringIntro, numberC) {
+    let bufferString = "";
+    if (numberC >= 0) {
+        for (let i = 0; i < numberC; i++) {
+            bufferString += stringIntro;
+        }
+    } else {
+        bufferString = "ERROR";
+    }
+    return bufferString;
 };
 
 // Do not edit below this line
