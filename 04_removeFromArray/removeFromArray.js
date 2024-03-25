7d060f224b40ad65eb783ffe35e10a147ca4ddb8
Incultavulpes <fernando.revenga@gmail.com>
2024-03-25 20:49:41 +0100

    Do exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayIntro, ...theArgs) {
+    for (let argument of theArgs) {
+        arrayIntro = removeBasicFromArray(arrayIntro, argument);
+    }
+    return arrayIntro;
 };
+
+function removeBasicFromArray (arrayIntro, arrayElementRemove) {
+    let count = 0;
+    for (let i = 0; i < arrayIntro.length; i++) {
+        if (arrayIntro[i - count] === arrayElementRemove) {
+            arrayIntro.splice(i - count, 1);
+            count++;
+        }
+    }
+    return arrayIntro;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
