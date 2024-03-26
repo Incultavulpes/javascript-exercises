36b8edbc93ea8f8853116ee20464d48f5ac6c2d9
Incultavulpes <fernando.revenga@gmail.com>
2024-03-26 11:27:48 +0100

    Do exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(yearNumber) {
+    let realLeapYear = true;
+    if ((yearNumber % 4) === 0) {
+        if ((yearNumber % 100) !== 0 || (((yearNumber % 100) === 0) && ((yearNumber % 400) === 0))) {
+            realLeapYear = true;
+        } else {
+            realLeapYear = false;
+        }
+    } else {
+        realLeapYear = false;
+    }
+    return realLeapYear;
 };
 
 // Do not edit below this line
