df660bf66aae29120f346daa3af269cd5799be45
Incultavulpes <fernando.revenga@gmail.com>
2024-03-26 10:59:18 +0100

    Do exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(lowerBound, upperBound) {
+    let sum = 0;
+    let bufferTempTrueUpper = 0;
+    let bufferTempTrueLower = 0;
+    if (upperBound < 0 || lowerBound < 0) {
+        sum = "ERROR";
+    } else if (typeof upperBound != "number" || typeof lowerBound != "number") {
+        sum = "ERROR";
+    } else {
+        if (upperBound < lowerBound) {
+            bufferTempTrueLower = upperBound;
+            bufferTempTrueUpper = lowerBound;
+            upperBound = bufferTempTrueUpper;
+            lowerBound = bufferTempTrueLower;
+        }
+        for (let i = lowerBound; i <= upperBound; i++) {
+            sum += i;
+        }   
+    }
+    return sum;
 };
 
 // Do not edit below this line
