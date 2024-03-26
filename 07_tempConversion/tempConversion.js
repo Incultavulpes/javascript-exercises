8c092e876ab8f1aa64b09080eb004c55c1eeed9a
Incultavulpes <fernando.revenga@gmail.com>
2024-03-26 11:57:28 +0100

    Do exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempNumber) {
+  tempNumber = (tempNumber - 32) * (5 / 9);
+  tempNumber = Math.round(tempNumber * 10) / 10;
+  return tempNumber;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempNumber) {
+  tempNumber = (tempNumber * (9/5)) + 32;
+  tempNumber = Math.round(tempNumber * 10) / 10;
+  return tempNumber;
 };
 
 // Do not edit below this line
