9e4ece157a1ac1d4dd9dea9bc0d3cf46070e1a49
Incultavulpes <fernando.revenga@gmail.com>
2024-05-08 21:00:04 +0200

    Do exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,40 @@
-const palindromes = function () {
+const remove = function (string) {
+    arstring = string.split("");
+    arstring = arstring.filter((element) => {
+        if (element === ".") {
+            return false;
+        } else if (element === ",") {
+            return false;
+        } else if (element === ":") {
+            return false;
+        } else if (element === ";") {
+            return false;
+        } else if (element === "!") {
+            return false;
+        } else if (element === "?") {
+            return false;
+        } else if (element === " ") {
+            return false;
+        } else {
+            return true;
+        }
+    });
+    return arstring.join("");
+};
 
+const palindromes = function (string) {
+    let palin = true;
+    string = string.toLowerCase();
+    string = remove(string);
+    arstring = string.split("");
+    arstring = arstring.reverse();
+    arstring = arstring.join("");
+    if (string === arstring) {
+        palin = true;
+    } else {
+        palin = false;
+    }
+    return palin;
 };
 
 // Do not edit below this line
