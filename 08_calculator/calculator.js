f8206f15dbf9a3d5bee86f9c61a14674d7daac50
Incultavulpes <fernando.revenga@gmail.com>
2024-05-08 20:04:52 +0200

    Do exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(c, d) {
+  return c - d;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+  result = arr.reduce((accumulator, currentItem) => accumulator + currentItem, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 0;
+  result = arr.reduce((accumulator, currentItem) => accumulator * currentItem, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(e, f) {
+  return Math.pow(e, f);
 };
 
-const factorial = function() {
-	
+const factorial = function(g) {
+  let result = 1;
+  if (g === 0) {
+    return result;
+  } else if (g < 0) {
+    return "No negative factorial has been defined";
+  } else {
+    for (let i = g; i > 0; i--) {
+      result = result * i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
